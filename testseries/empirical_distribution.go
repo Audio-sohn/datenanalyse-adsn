15d156e56dfa218f72b1e98ea31fb9d95c204936
Audio-sohn <info@audiosohn.com>
2025-01-14 08:11:20 +0100

    empirical distribution done

--- testseries/empirical_distribution.go
+++ testseries/empirical_distribution.go
@@ -5,13 +5,27 @@ package testseries
 // entsprechende Wert der empirischen Verteilungsfunktion steht.
 func EmpiricalDistribution(relativeFreqs []float64) []float64 {
 	emp := make([]float64, len(relativeFreqs))
-	// TODO
+
+	// temp for storing sum
+	var sum float64 = 0
+
+	// iterate through relativeFreqs array
+	for i, speci := range relativeFreqs {
+
+		sum += speci
+		emp[i] = sum
+
+	}
+
 	return emp
 }
 
 // Distribution erwartet eine Liste mit ganzzahligen Messwerten.
 // Die Funktion liefert eine Liste mit den Werten der empirischen Verteilungsfunktion.
 func Distribution(values []int) []float64 {
-	// TODO
-	return []float64{}
+
+	rel := RelativeFrequencies(values)
+	emp := EmpiricalDistribution(rel)
+
+	return emp
 }
